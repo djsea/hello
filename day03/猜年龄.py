e406689a108d6c9d04d1907d0ac8b64fc07672bf
嘿嘎嘎 <djsea@139.com>
2019-10-28 14:44:18 +0800

    2019/10/28 猜年龄

--- day03/猜年龄.py
+++ day03/猜年龄.py
@@ -3,4 +3,20 @@
 #   name:嘿嘎嘎
 #   猜年龄游戏
 
-count = 1
+count = 0
+oldboy = 30
+while True:
+    print("这是第%d次" % count)
+    count += 1
+    age = int(input("猜我多大了："))
+    if age == oldboy:
+        print("你猜对了")
+        break
+    else:
+        if age > oldboy:
+            print("我还年轻")
+            continue
+        else:
+            print("我很老了！")
+            continue
+print("你猜了%s次" % count)
